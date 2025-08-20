6e9c5ff6f1a3cd981e679a4bbc895a6220bb83a2
advanceplays <adhyayanw.24277@obhs.school.nz>
2025-08-21 09:32:27 +1200

    input and functions added

---
+++ heads_tails.py
@@ -0,0 +1,13 @@
+import random
+
+def heads_tails():
+    user_score = 0
+
+
+
+
+#main porgram
+print("Hi! Welcome to my Heads and Tails game")
+first_name-str(input("What is your name"))
+age = int(input("What is your age"))
+heads_tails() #this calls up the function
